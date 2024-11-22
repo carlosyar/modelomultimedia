bfa27c44ee8774839070481823aeaaca8f5375ff
carlosyar <73135273+carlosyar@users.noreply.github.com>
2024-11-22 14:57:53 -0500

    app.py

--- app.py
+++ app.py
@@ -11,13 +11,21 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 
+# Global variable to store the model and prevent reloading
+model = None
+
+# Function to load the model only when needed
+def load_trained_model():
+    global model
+    if model is None:
+        model = load_model('model3.keras')
+        print("Modelo cargado.")
+    return model
+
 def crear_app():
     app = Flask(__name__)
-    
-    # Load the model
-    model = load_model('model3.keras')
-    
-    # Class names
+
+    # Class names (you can keep this global or move it to the function)
     class_names = [
         'Bean', 'Bitter_Gourd', 'Bottle_Gourd', 'Brinjal', 
         'Broccoli', 'Cabagge', 'Capsicum', 'Carrot', 
@@ -39,15 +47,16 @@ def crear_app():
         file = request.files['file']
         if file.filename == '':
             return "No selected file"
-    
+        
         # Read the image file and convert it
-      # Read the image file and convert it
         img = Image.open(file.stream)
         img = img.resize((150, 150))  # Resize to 150x150
         img_array = image.img_to_array(img)
         img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize
 
-    
+        # Load the model only when making the prediction (Lazy Loading)
+        model = load_trained_model()
+
         # Perform the prediction
         preds = model.predict(img_array)
         predicted_class_index = np.argmax(preds[0])  # Get the index of the class with the highest probability
@@ -62,5 +71,4 @@ def crear_app():
 
 if __name__ == '__main__':
     app = crear_app()
-    app.run()
-
+    app.run(debug=True)
